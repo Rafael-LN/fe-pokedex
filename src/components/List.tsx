86f1d8e1f93baa27e6d341573fb366c23f7cc5a6
Rafael-LN <rafaelnunes3092@gmail.com>
2024-04-16 20:34:15 +0100

    Create list component to display list of Pokémon

---
+++ src/components/List.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Picture from './Picture';
+import { Pokemon } from "../models";
+import "../styles/List.css";
+
+interface ListProps {
+    pokemons: Pokemon[];
+}
+
+const List: React.FC<ListProps> = ({ pokemons }) => {
+    return (
+        <div>
+            <h2>Pokémon List</h2>
+            <ul>
+                {pokemons.map((pokemon, index) => (
+                    <li key={index}>
+                        <div className={"item"}>
+                            <Picture pictureUrl={pokemon.pictureUrl} />
+                            <span>{pokemon.name}</span>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default List;
