304950d6484be5b9f621c8cbd9c01848e0fa8d04
Rafael-LN <rafaelnunes3092@gmail.com>
2024-04-17 19:46:32 +0100

    Create strings utils file
    * Create function to capitalize a string

---
+++ src/utils/strings.ts
@@ -0,0 +1,7 @@
+export function capitalize(str: string): string {
+    // Ensure the string is not empty
+    if (!str) return str;
+
+    // Capitalize the first letter and concatenate with the rest of the string
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
