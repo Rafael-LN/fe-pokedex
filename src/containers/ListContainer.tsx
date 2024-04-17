58d0cde006ab06a5276f3493ba53924d0ec05de0
Rafael-LN <rafaelnunes3092@gmail.com>
2024-04-17 19:58:37 +0100

    Refactor ListContainer to use Pagination context

--- src/containers/ListContainer.tsx
+++ src/containers/ListContainer.tsx
@@ -1,30 +1,41 @@
 // containers/PokemonListContainer.tsx
-import React, {useEffect, useState} from 'react';
+import React, {Fragment, useEffect, useState} from 'react';
 import List from '../components/List';
-import {getPokemon} from '../services/pokemonService';
+import {getPokemonList} from '../services/pokemonService';
 import {PokemonList} from '../models';
+import '../styles/Pagination.css'
+import Pagination from "../components/Pagination";
+import {usePagination} from "../context/PaginationContext";
 
-const PokemonListContainer: React.FC = () => {
+const PAGE_SIZE = 20 as const;
+
+export function PokemonListContainer() {
+    const {currentPage} = usePagination();
     const [pokemonList, setPokemonList] = useState<PokemonList[]>([]);
+    const [totalPages, setTotalPages] = useState(0);
 
     useEffect(() => {
-        const fetchPokemonList = async () => {
-            try {
-                const pokemonNames = ['pikachu', 'bulbasaur', 'charmander']; // Example list of Pokémon names
-                const pokemonDataPromises = pokemonNames.map(name => getPokemon(name)); // Pass cache functions to getPokemon
-                const pokemonData = await Promise.all(pokemonDataPromises);
-                setPokemonList(pokemonData);
-            } catch (error) {
-                console.error(error);
-            }
-        };
+        fetchPokemonData();
+    }, [currentPage]); // Fetch data whenever currentPage changes
 
-        fetchPokemonList();
-    }, []); // Add cache functions to dependency array
+    const fetchPokemonData = async () => {
+        try {
+            const offset = (currentPage - 1) * PAGE_SIZE;
+            const pokemonList = await getPokemonList(PAGE_SIZE, offset);
+            const totalPages = Math.ceil(pokemonList.length / PAGE_SIZE);
+
+            setPokemonList(pokemonList);
+            setTotalPages(totalPages);
+        } catch (error) {
+            console.error('Error fetching Pokémon data:', error);
+        }
+    };
+
 
     return (
-        <List pokemons={pokemonList}/>
+        <Fragment>
+            <List pokemons={pokemonList}/>
+            <Pagination/>
+        </Fragment>
     );
-};
-
-export default PokemonListContainer;
+}
