5d30f2d4511b6d822ed56fd3403729d20e8bdb43
Rafael-LN <rafaelnunes3092@gmail.com>
2024-04-17 19:45:09 +0100

    Refactor list component to display pokeball if Pokémon was caught

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import Picture from './Picture';
-import { PokemonList } from "../models";
+import { type PokemonList} from "../models";
 import "../styles/List.css";
 
-interface ListProps {
+type ListProps = {
     pokemons: PokemonList[];
 }
 
-const List: React.FC<ListProps> = ({ pokemons }) => {
+const List: React.FC<ListProps> = ({pokemons}) => {
     return (
-        <div>
+        <Fragment>
             <h2>Pokémon List</h2>
-            <ul>
-                {pokemons.map((pokemon, index) => (
-                    <li key={index}>
-                        <div className={"item"}>
-                            <Picture url={pokemon.pictureUrl} alt={pokemon.name} />
-                            <span>{pokemon.name}</span>
-                        </div>
-                    </li>
+            <div className={"list"}>
+                {pokemons.map((pokemon) => (
+                    <div key={`k-${pokemon.name}`} className={"item"}>
+                        <Picture url={pokemon.pictureUrl} alt={pokemon.name}/>
+                        <span>{pokemon.name}</span> {pokemon.caught && <Picture
+                        url={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png"}
+                        alt={"poke-ball"}/>}
+                    </div>
                 ))}
-            </ul>
-        </div>
+            </div>
+        </Fragment>
     );
 };
 
