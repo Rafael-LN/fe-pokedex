6cbaa0cda0927aff4f119a7cd86403decf82a5ed
Rafael-LN <rafaelnunes3092@gmail.com>
2024-04-17 19:57:56 +0100

    Refactor pokemonService file

--- src/services/pokemonService.ts
+++ src/services/pokemonService.ts
@@ -1,26 +1,28 @@
-// services/pokemonService.ts
 import axios from 'axios';
 import {PokemonList} from '../models';
+import {capitalize} from "../utils";
 
-//export const getPokemon = async (pokemonNameOrId: string, pokemonCache: Record<string, Pokemon>, setPokemonCache: React.Dispatch<React.SetStateAction<Record<string, Pokemon>>>): Promise<Pokemon> => {
-export const getPokemon = async (pokemonNameOrId: string): Promise<PokemonList> => {
+export const getPokemonList = async (limit: number = 30, offset: number = 0): Promise<PokemonList[]> => {
     /*if (pokemonCache[pokemonNameOrId]) {
         return pokemonCache[pokemonNameOrId];
     }*/
 
     try {
-        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonNameOrId}`);
-        // Update the cache
-        /*setPokemonCache(prevCache => ({
-            ...prevCache,
-            [pokemonNameOrId]: pokemon,
-        }));*/
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
+        const pokemonList = response.data.results;
 
-        return {
-            name: response.data.name,
-            pictureUrl: response.data.sprites.front_default,
-        };
+        const pokemonDetailsPromises = pokemonList.map(async (pokemon: any) => {
+            const detailsResponse = await axios.get(pokemon.url);
+            const { name, sprites } = detailsResponse.data;
+            const pictureUrl = sprites.front_default || ''; // Use the front default sprite URL or an empty string if not available
+            return {
+                name: capitalize(name),
+                pictureUrl,
+            };
+        });
+
+        return Promise.all(pokemonDetailsPromises);
     } catch (error) {
-        throw new Error(`Failed to fetch Pokemon data for ${pokemonNameOrId}`);
+        throw new Error(`Failed to fetch Pokémon data`);
     }
 };
