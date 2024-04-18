c319144618d82a42773cb0a7f0e442430d5bd4da
Rafael-LN <rafaelnunes3092@gmail.com>
2024-04-18 17:39:53 +0100

    Create PokemonCard component

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,21 @@
+import {PokemonList} from "../models";
+import {POKEBALL_IMAGE_URL} from "../constants";
+
+type PokemonCardProps = {
+    pokemon: PokemonList;
+}
+
+export default function PokemonCard({pokemon}: PokemonCardProps) {
+    const {name, url, caught, image, pokedexNumber} = pokemon;
+    return (
+        <div key={`${name}-${pokedexNumber}`} className="card" style={{width:"15rem"}}>
+            <img className="card-img-top " src={image} alt={name}/>
+            <div className="card-body">
+                <span className="text-capitalize">{name}</span> {caught && <img
+                src={POKEBALL_IMAGE_URL}
+                alt={"poke-ball"}/>}
+            </div>
+
+        </div>
+    )
+}
