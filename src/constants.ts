5d4872598f076070d5ad5327970b39134fd3603d
Rafael-LN <rafaelnunes3092@gmail.com>
2024-04-18 15:50:49 +0100

    Create constants file for urls

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const POKEMON_API_BASE_URL = "https://pokeapi.co/api/v2";
+export const POKEMON_API_POKEMON_URL = `${POKEMON_API_BASE_URL}/pokemon`;
+export const POKEMON_IMAGES_BASE_URL =
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork";
+export const POKEBALL_IMAGE_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png"
