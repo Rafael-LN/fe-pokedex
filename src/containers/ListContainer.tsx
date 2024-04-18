470e05060e03721880e13880110f137063dcf665
Rafael-LN <rafaelnunes3092@gmail.com>
2024-04-18 16:08:45 +0100

    Refactor ListContainer to use custom hook usePokemons

--- src/containers/ListContainer.tsx
+++ src/containers/ListContainer.tsx
@@ -1,41 +1,19 @@
-// containers/PokemonListContainer.tsx
-import React, {Fragment, useEffect, useState} from 'react';
 import List from '../components/List';
-import {getPokemonList} from '../services/pokemonService';
-import {PokemonList} from '../models';
 import '../styles/Pagination.css'
-import Pagination from "../components/Pagination";
-import {usePagination} from "../context/PaginationContext";
+import usePokemons from "../hooks/usePokemons";
 
-const PAGE_SIZE = 20 as const;
 
 export function PokemonListContainer() {
-    const {currentPage} = usePagination();
-    const [pokemonList, setPokemonList] = useState<PokemonList[]>([]);
-    const [totalPages, setTotalPages] = useState(0);
-
-    useEffect(() => {
-        fetchPokemonData();
-    }, [currentPage]); // Fetch data whenever currentPage changes
-
-    const fetchPokemonData = async () => {
-        try {
-            const offset = (currentPage - 1) * PAGE_SIZE;
-            const pokemonList = await getPokemonList(PAGE_SIZE, offset);
-            const totalPages = Math.ceil(pokemonList.length / PAGE_SIZE);
-
-            setPokemonList(pokemonList);
-            setTotalPages(totalPages);
-        } catch (error) {
-            console.error('Error fetching Pokémon data:', error);
-        }
-    };
-
+    const {pokemons, hasMorePokemon, fetchNextPage} = usePokemons();
 
     return (
-        <Fragment>
-            <List pokemons={pokemonList}/>
-            <Pagination/>
-        </Fragment>
+        <>
+            <List pokemons={pokemons}/>
+            {hasMorePokemon ? (
+                <button onClick={fetchNextPage}>
+                    Load more Pokemon
+                </button>
+            ) : null}
+        </>
     );
 }
