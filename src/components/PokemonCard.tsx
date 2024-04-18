0fb7811b08bd201cad9c9e9de42f0262b10d2d36
Rafael-LN <rafaelnunes3092@gmail.com>
2024-04-19 00:44:48 +0100

    Add Link from React router to redirect to Pokémon details when a Pokémon is clicked

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,5 +1,6 @@
 import {PokemonList} from "../models";
 import {POKEBALL_IMAGE_URL} from "../constants";
+import {Link} from "react-router-dom";
 
 type PokemonCardProps = {
     pokemon: PokemonList;
@@ -8,14 +9,15 @@ type PokemonCardProps = {
 export default function PokemonCard({pokemon}: PokemonCardProps) {
     const {name, url, caught, image, pokedexNumber} = pokemon;
     return (
-        <div key={`${name}-${pokedexNumber}`} className="card mb-4" style={{width:"15rem"}}>
-            <img className="card-img-top " src={image} alt={name}/>
-            <div className="card-body">
-                <span className="text-capitalize">{name}</span> {caught && <img
-                src={POKEBALL_IMAGE_URL}
-                alt={"poke-ball"}/>}
+        <Link to={`pokemon/${pokemon.name}`} className="text-decoration-none">
+            <div key={`${name}-${pokedexNumber}`} className="card mb-4" style={{width: "15rem"}}>
+                <img className="card-img-top " src={image} alt={name}/>
+                <div className="card-body">
+                    <span className="text-capitalize">{name}</span> {caught && <img
+                    src={POKEBALL_IMAGE_URL}
+                    alt={"poke-ball"}/>}
+                </div>
             </div>
-
-        </div>
+        </Link>
     )
 }
