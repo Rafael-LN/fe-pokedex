c101a5dd335fe972c72f00eacf357850a18c0ac0
Rafael-LN <rafaelnunes3092@gmail.com>
2024-04-19 00:43:38 +0100

    Add simple card to PokemonDetail component

--- src/components/PokemonDetail.tsx
+++ src/components/PokemonDetail.tsx
@@ -1,3 +1,34 @@
+import {useParams} from "react-router-dom";
+import usePokemon from "../hooks/usePokemon";
+
 export default function PokemonDetail() {
 
+    const {name} = useParams();
+
+    const {pokemon, isLoading} = usePokemon({name});
+
+    console.log(pokemon)
+    return (
+        <>
+            {
+                isLoading ?
+                    (
+                        <div>Loading...</div>
+                    )
+                    :
+                    (
+                        <div className="container" style={{backgroundColor: `${pokemon?.color}`}}>
+                            <div key={`${pokemon?.name}-${pokemon?.id}`} className="card mb-4" style={{width: "15rem"}}>
+                                <img className="card-img-top "
+                                     src={pokemon?.sprites.other["official-artwork"].front_default}
+                                     alt={name}/>
+                                <div className="card-body">
+                                    <span className="text-capitalize">{pokemon?.name}</span>
+                                </div>
+                            </div>
+                        </div>
+                    )
+            }
+        </>
+    )
 }
