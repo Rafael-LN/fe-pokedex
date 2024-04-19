94855ae39d02c552ff22a3ea2ad5ae1c2fb6cd29
Rafael-LN <rafaelnunes3092@gmail.com>
2024-04-19 21:28:59 +0100

    Add Type to Pokémon Info

--- src/components/PokemonInfo.tsx
+++ src/components/PokemonInfo.tsx
@@ -1,5 +1,7 @@
-import {PokemonDetails} from "../models";
-import {Card} from "react-bootstrap";
+import {PokemonDetails, PokemonType} from "../models";
+import {Card, Col, Row} from "react-bootstrap";
+import {POKEBALL_IMAGE_URL} from "../constants";
+import {capitalize} from "../utils";
 
 interface PokemonInfoProps {
     pokemon: PokemonDetails
@@ -10,8 +12,18 @@ export function PokemonInfo({pokemon}: PokemonInfoProps) {
         <Card>
             <Card.Body>
                 <Card.Title className="text-capitalize">{pokemon.name}</Card.Title>
-                <Card.Text>Height: {pokemon.height}</Card.Text>
-                <Card.Text>Weight: {pokemon.weight}</Card.Text>
+                <Row>
+                    <Col>
+                        <Card.Text> Height: {pokemon.height} </Card.Text>
+                        <Card.Text> Weight: {pokemon.weight} </Card.Text>
+                    </Col>
+                    <Col>
+                        {pokemon.types.map(({type}: PokemonType) =>
+                            <Card.Text key={type.name}>Type: {capitalize(type.name)}</Card.Text>
+                        )}
+                    </Col>
+                </Row>
+
             </Card.Body>
         </Card>
     );
