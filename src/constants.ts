6d1e4eb0dde1ada25c2bfd5b153f637595eab910
Rafael-LN <rafaelnunes3092@gmail.com>
2024-04-21 23:19:37 +0100

    Add url to types

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,6 @@
 export const POKEMON_API_BASE_URL = "https://pokeapi.co/api/v2";
 export const POKEMON_API_POKEMON_URL = `${POKEMON_API_BASE_URL}/pokemon`;
+export const POKEMON_API_TYPE_URL = `${POKEMON_API_BASE_URL}/type`;
 export const POKEMON_IMAGES_BASE_URL =
     "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork";
 export const POKEBALL_IMAGE_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png"
