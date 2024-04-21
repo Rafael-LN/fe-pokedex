4d0876bb64d22a655ba736f9f801b6c0a24f34bf
Rafael-LN <rafaelnunes3092@gmail.com>
2024-04-21 11:06:06 +0100

    Update PokemonStat display

--- src/components/PokemonStats.tsx
+++ src/components/PokemonStats.tsx
@@ -1,5 +1,5 @@
 import {PokemonStat} from "../models";
-import {Card, Col, ListGroup, ProgressBar, Row} from "react-bootstrap";
+import {Card, ProgressBar} from "react-bootstrap";
 
 interface PokemonStatsProps {
     stats: PokemonStat[]
@@ -8,18 +8,17 @@ interface PokemonStatsProps {
 export default function PokemonStats({stats}: PokemonStatsProps) {
 
     return (
-        <Card className="w-75">
-            <Card.Body>
+        <Card className="w-50">
+            <Card.Body >
                 {stats.map((stat) => (
-
-                    <ListGroup.Item className="d-flex justify-content-end align-items-center">
+                    <>
+                    <div className="d-flex mb-2">
                         <span key={stat.stat.name}
                                    className="text-capitalize fw-semibold">{stat.stat.name}</span>
-                        <div className="ms-3 vr"></div>
                         <span className="mx-3">{stat.base_stat}</span>
-                        <ProgressBar className="w-75 " now={stat.base_stat} max={255}/>
-                    </ListGroup.Item>
-
+                    </div>
+                        <ProgressBar className="mb-2"  now={stat.base_stat} max={255}/>
+                    </>
                 ))}
             </Card.Body>
         </Card>
