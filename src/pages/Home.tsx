6e51578b54218fb8fd904eacee58f5a5f1f05289
Rafael-LN <rafaelnunes3092@gmail.com>
2024-04-23 04:00:50 +0100

    Style refactor for Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,20 @@
 import {Link} from "react-router-dom";
-import {Button, Container} from "react-bootstrap";
+import {Button, Container, Row} from "react-bootstrap";
 
 
 export default function Home() {
     return (
-        <Container className="align-items-center">
-            <h1>Welcome to the Pokémon App</h1>
-            <Link to="/gallery">
-                <Button>Gallery</Button>
-            </Link>
-            <Link to="/pokedex">
-                <Button variant="danger">Pokedex</Button>
-            </Link>
+        <Container className={`text-center pt-4`}>
+            <h1 className={`my-4`}>Welcome to the Pokémon App</h1>
+            <Row>
+                <Link className={`my-3`} to="/gallery">
+                    <Button size={`lg`} variant={`info`}>Open the Gallery</Button>
+                </Link>
+                <Link to="/pokedex">
+                    <Button size={`lg`} variant={`secondary`}>Check your Pokedex</Button>
+                </Link>
+            </Row>
+
         </Container>
     )
 }
