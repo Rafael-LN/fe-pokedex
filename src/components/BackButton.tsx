a688b1f6c364d0fa134e0db51556673444fcfdbd
Rafael-LN <rafaelnunes3092@gmail.com>
2024-04-23 13:38:46 +0100

    Create Back Button

---
+++ src/components/BackButton.tsx
@@ -0,0 +1,20 @@
+import {useNavigate} from "react-router-dom";
+import {Button} from "react-bootstrap";
+
+interface BackButtonProps {
+    margin?: string
+}
+
+export function BackButton({margin}: BackButtonProps) {
+    const navigate = useNavigate();
+
+    const goBack = () => {
+        navigate(-1); // Navigate back one step in history
+    };
+
+    return (
+        <Button onClick={goBack} variant={`outline-secondary`} className={`${margin}`}>
+            &#8592; Back
+        </Button>
+    );
+}
